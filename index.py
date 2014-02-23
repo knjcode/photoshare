dbd1d7d6fa4943046ef434d690e0069a709efaa8
knjcode <knjcode@gmail.com>
2014-02-23 22:38:00 +0900

    Cookie test

--- index.py
+++ index.py
@@ -3,9 +3,14 @@
 
 import os
 import glob
+import datetime
+import Cookie
 import csv
 
 pagetitle = os.path.basename(os.getcwd())
+sc = Cookie.SimpleCookie(os.environ.get('HTTP_COOKIE',''))
+expires = datetime.datetime.now() + datetime.timedelta(days=1)
+c_name = sc.get('sort').value if sc.has_key('sort') else 'nocookie'
 
 try:
     reader = csv.reader(open("sort_datetime.csv"))
@@ -19,12 +24,15 @@ print "<html>"
 print "<head>"
 print "<meta charset=\"utf-8\">"
 print "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+print "<title>%s</title>" % pagetitle
 print "<link rel=\"stylesheet\" href=\"//photo.guit.net/css/grid.css\" />"
 print "<link rel=\"stylesheet\" href=\"//photo.guit.net/css/lightbox.css\" />"
 print "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>"
 print "<script src=\"//photo.guit.net/js/jquery.cookie.js\"></script>"
 print "<script src=\"//photo.guit.net/js/lightbox-2.6.min.js\"></script>"
-print "<title>%s</title>" % pagetitle
+print "<script type=\"text/javascript\">"
+print ""
+print "</script>"
 print "</head>"
 print "<body>"
 print "<div class=\"grid\">"
@@ -37,6 +45,10 @@ for row in reader:
     print "<div class=\"title\">%s</div>" % infile
     print "</a></div>"
 
+print """
+<ul>
+<li>get COOKIE value: {0}</li>
+</ul>""".format(c_name)
 print "</div>"
 print "</body>"
 print "</html>"
