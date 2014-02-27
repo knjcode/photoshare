6995f6e8a53df58a7227010a798c1de173213ff3
knjcode <knjcode@gmail.com>
2014-02-27 22:26:58 +0900

    Add event selected listbox

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,23 @@
-$(function(){
-    $('a img').hover(function(){
-        $(this).fadeTo("normal",0.6);
-    },function(){
-        $(this).fadeTo("normal",1.0);
+$(document).ready(function(){
+    $('a img').hover(
+        function(){
+            $(this).fadeTo("fast",0.6);
+        },
+        function(){
+            $(this).fadeTo("fast",1.0);
+        }
+    );
+    $('p').text($.cookie("sort"));
+
+    sort = $("#sort");
+    $('#sort').bind('change',function(){
+        switch (sort.val()) {
+            case "datetime": $.cookie("sort","datetime"); $('p').text($.cookie("sort")); break;
+            case "datetime_reverse": $.cookie("sort","datetime_reverse"); $('p').text($.cookie("sort")); break;
+            case "filedate": $.cookie("sort","filedate"); $('p').text($.cookie("sort")); break;
+            case "filedate_reverse": $.cookie("sort","filedate_reverse"); $('p').text($.cookie("sort")); break;
+            default : $.cookie("sort","unknown");
+        }
     });
+
 });
